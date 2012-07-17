0949987a32f2cd3c22e6f0f1057ac3eb06c75b3e
Cody Gillmer <garglesnuffigus@gmail.com>
2012-07-17 01:36:19 -0700

    Re-organizing, adding skeleton files.

--- Gemini/Properties/AssemblyInfo.cs
+++ Gemini/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gemini")]
-[assembly: AssemblyProduct("Gemini")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gemini Engine")]
+[assembly: AssemblyDescription("General purpose game engine.")]
+[assembly: AssemblyCompany("Creative³ Games")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
